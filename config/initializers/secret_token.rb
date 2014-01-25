af277fcf7dce966d29bf9f011820a0cd3736cd0d
lawrence <yuliang1987@gmail.com>
2014-01-25 10:18:53 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StartupDigest::Application.config.secret_key_base = 'e34ce8791fb23c7fd08bdccf4617b7d66d48a4f194f7c759f45601ea12f6df6979aac0c5d72a4bedfa0453cb3e3c7fe44a0876fafe970ab5e7adee712d5d6562'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StartupDigest::Application.config.session_store :cookie_store, key: '_startup_digest_session'
