37c0cc0edc754547a5844f8a56b04952745536de
lawrence <yuliang1987@gmail.com>
2014-02-05 11:53:04 +0800

    add gravatar and post/comment info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 	before_action :get_categories
+	before_action :require_user, only:[:edit, :update]
+	before_action :find_user, only:[:show, :edit, :update]
 
 	def new
 		@user = User.new
@@ -17,9 +19,30 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@posts = @user.posts
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "更新成功."
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end 
+
+
 	private
 
 		def user_params
 			params.require(:user).permit(:username, :email, :password, :password_confirmation)
 		end 
+
+		def find_user
+			@user = User.find(params[:id])
+		end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :user
 
-	validates :body, presence: true, length: {minimum: 5}
+	validates :body, presence: true
 
 end
