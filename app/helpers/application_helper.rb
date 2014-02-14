c598e0200d792b35b222a9df7d45238f51593c57
lawrence <yuliang1987@gmail.com>
2014-02-14 09:29:34 +0800

    Add markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
 	def display_date(dt)
 		dt.strftime("%d/%m/%Y") 
 	end
+
+	def markdown(text)
+		renderOptions = {hard_wrap: true, filter_html: true}
+    markdownOptions = {autolink: true, fenced_code_blocks: true, no_intra_emphasis: true, strikethrough: true}
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderOptions), markdownOptions)
+    markdown.render(text).html_safe
+	end
 end
