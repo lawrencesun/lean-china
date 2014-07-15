9d8ba833cce05fe4f79ffece287e0fbc4a0069f0
lawrence <yuliang1987@gmail.com>
2014-07-15 13:12:25 +0800

    add nprogress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+//= require nprogress
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
